552f05324affb019c9da0744e54b39e5dcc8468e
awei <awei@awei.com>
2020-10-20 23:22:57 +0800

    调整http server的代码结构，http server已经成型

--- WinControlCpp/WinControl/libevent_http/libevent_http.cpp
+++ WinControlCpp/WinControl/libevent_http/libevent_http.cpp
@@ -2,101 +2,160 @@
 #include "libevent_http.h"
 #include "../LogFile.h"
 #include "json/reader.h"
+#include <string>
+
+using namespace std;
 
 CHttpServerHandle::CHttpServerHandle()
 {
-	httpd = NULL;
+	m_pEvHttpd = NULL;
 }
 
 CHttpServerHandle::~CHttpServerHandle()
 {
 }
 
-void CHttpServerHandle::RootHandler(struct evhttp_request *req, void *arg)
+//Http其他方式使用HttpDefaultHandler处理
+void CHttpServerHandle::HttpDefaultHandler(string& strReplyBuffer, struct evhttp_request *pEvHttpRequest, void *pArg)
 {
-	struct evbuffer *buf = evbuffer_new();
-	if (!buf)
-	{
-		GetLogFileHandle().ErrorLog("%s %d failed to create response buffer\n", __FUNCTION__, __LINE__);
-		return;
-	}
-
 	Json::Value jsonReply;
 	jsonReply["ret"] = -1;
-	jsonReply["error_message"] = string_To_UTF8("unknown error").c_str();
+	jsonReply["error_message"] = string_To_UTF8("http post method is only supported").c_str();
+
+	strReplyBuffer = jsonReply.toStyledString();
+}
+
+//Get方法处理函数
+void CHttpServerHandle::HttpGetHandler(string& strReplyBuffer, struct evhttp_request *pEvHttpRequest, void *pArg)
+{
+	Json::Value jsonReply;
+	jsonReply["ret"] = -1;
+	jsonReply["error_message"] = string_To_UTF8("http get method is not supported").c_str();
+
+	strReplyBuffer = jsonReply.toStyledString();
+}
+
+//将不同的Http request请求分发到不同的函数处理
+void CHttpServerHandle::WebRequestDispatch(UINT32 uiID, Json::Value& jsonJarg, string& strReplyBuffer)
+{
+	switch (uiID)
+	{
+	case 900001:
+
+		break;
+	default:
+		break;
+	}
+}
+
+//处理Web的request请求并得到返回结果
+void CHttpServerHandle::WebRequestParser(string& strRequestBuffer, string& strReplyBuffer)
+{
 	std::string strError;
-	switch(evhttp_request_get_command(req))
+	Json::Value jsonReply;
+	try
+	{
+		Json::Reader jsonReader;
+		Json::Value jsonDataValue;
+		if (jsonReader.parse(strRequestBuffer, jsonDataValue))
+		{
+			UINT32 uiID = jsonDataValue["id"].asInt();
+			Json::Value jsonJarg = jsonDataValue["arg"];
+			WebRequestDispatch(uiID, jsonJarg, strReplyBuffer);
+		}
+		else
+		{
+			jsonReply["ret"] = -1;
+			jsonReply["error_message"] = string_To_UTF8("wrong json data.").c_str();
+			strReplyBuffer = jsonReply.toStyledString();
+		}
+	}
+	catch (const char* e)
+	{
+		strError = e;
+		jsonReply["ret"] = -1;
+		jsonReply["error_message"] = string_To_UTF8(strError).c_str();
+		strReplyBuffer = jsonReply.toStyledString();
+	}
+}
+
+//获取post的json数据
+void CHttpServerHandle::GetHttpPostData(string& strDataBuffer, struct evhttp_request *pEvHttpRequest, void *pArg)
+{
+	struct evbuffer* pEvBuffer = evhttp_request_get_input_buffer(pEvHttpRequest);
+	UINT32 uiLeft = evbuffer_get_length(pEvBuffer);
+	while (left > 0)
+	{
+		char szBuffer[512] = { 0 };
+		int hasRead = evbuffer_remove(pEvBuffer, szBuffer, 512);
+		if (hasRead != -1)
+		{
+			uiLeft -= static_cast<UINT32>(hasRead);
+			strDataBuffer.append(szBuffer);
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+//Post方法处理函数
+void CHttpServerHandle::HttpPostHandler(string& strReplyBuffer, struct evhttp_request *pEvHttpRequest, void *pArg)
+{
+	string strRequestBuffer;
+	GetHttpPostData(strRequestBuffer, pEvHttpRequest, pArg);
+	if (strRequestBuffer.length() <= 0)
+	{
+		GetLogFileHandle().ErrorLog("%s %d failed to create response buffer\n", __FUNCTION__, __LINE__);
+		return;
+	}
+
+	WebRequestParser(strRequestBuffer, strReplyBuffer);
+}
+
+//特定URI回调函数
+void CHttpServerHandle::HttpRootHandler(struct evhttp_request *pEvHttpRequest, void *pArg)
+{
+	struct evbuffer *pEvBuffer = evbuffer_new();
+	if (!pEvBuffer)
+	{
+		GetLogFileHandle().ErrorLog("%s %d failed to create response buffer\n", __FUNCTION__, __LINE__);
+		return;
+	}
+
+	string strReplyBuffer;
+	switch (evhttp_request_get_command(pEvHttpRequest))
 	{
 	case EVHTTP_REQ_GET:
-		jsonReply["ret"] = -1;
-		jsonReply["error_message"] = string_To_UTF8("This request method is not supported").c_str();
-		evbuffer_add_printf(buf, jsonReply.toStyledString().c_str());
+		HttpGetHandler(strReplyBuffer, pEvHttpRequest, pArg);
 		break;
 	case EVHTTP_REQ_POST:
-		struct evbuffer* postBuffer = evhttp_request_get_input_buffer(req);
-		size_t left = evbuffer_get_length(postBuffer);
-		string buffer;
-		while (left > 0)
-		{
-			char cbuffer[512] = { 0 };
-			int hasRead = evbuffer_remove(postBuffer, cbuffer, 512);
-			if (hasRead != -1)
-			{
-				left -= static_cast<size_t>(hasRead);
-				buffer.append(cbuffer);
-			}
-			else
-			{
-				break;
-			}
-		}
-		try
-		{
-			Json::Reader reader;
-			Json::Value root;
-			if (reader.parse(buffer, root))
-			{
-				int id = root["id"].asInt();
-				Json::Value jarg = root["arg"];
-				jsonReply["ret"] = 0;
-				jsonReply["error_message"] = string_To_UTF8("").c_str();;
-			}
-			else
-			{
-				jsonReply["ret"] = -1;
-				jsonReply["error_message"] = string_To_UTF8("wrong data").c_str();
-			}
-		}
-		catch (const char* e)
-		{
-			strError = e;
-			jsonReply["ret"] = -1;
-			jsonReply["error_message"] = string_To_UTF8(strError).c_str();
-		}
-		evbuffer_add_printf(buf, jsonReply.toStyledString().c_str());
+		HttpPostHandler(strReplyBuffer, pEvHttpRequest, pArg);
+		break;
+	default:
+		HttpDefaultHandler(strReplyBuffer, pEvHttpRequest, pArg);
 		break;
 	}
 
-	const char* requri = evhttp_request_uri(req);
-	evhttp_send_reply(req, HTTP_OK, "OK", buf);
-
-	evbuffer_free(buf);
+	evbuffer_add_printf(pEvBuffer, strReplyBuffer.c_str());
+	evhttp_send_reply(pEvHttpRequest, HTTP_OK, "OK", pEvBuffer);
+	evbuffer_free(pEvBuffer);
 }
 
-void CHttpServerHandle::GenericHandler(struct evhttp_request *req, void *arg)
+//没有被特定URI回调则调用此函数 
+void CHttpServerHandle::HttpGenericHandler(struct evhttp_request *pEvHttpRequest, void *pArg)
 {
 	struct evbuffer *buf = evbuffer_new();
 	if (!buf)
 	{
-		//puts("failed to create response buffer");
 		GetLogFileHandle().ErrorLog("%s %d failed to create response buffer\n", __FUNCTION__, __LINE__);
 		return;
 	}
-	const char* requri = evhttp_request_uri(req);
-	//LOG_TOFILE("paycallback.log", "%s callback is \n%s\nrequesturi:%s\n", __FUNCTION__, req->uri, requri);
 
+	const char* requri = evhttp_request_uri(pEvHttpRequest);
 	evbuffer_add_printf(buf, "Requested: %s\n", requri);
-	evhttp_send_reply(req, HTTP_OK, "OK", buf);
+	evhttp_send_reply(pEvHttpRequest, HTTP_OK, "OK", buf);
 
 	evbuffer_free(buf);
 }
@@ -112,8 +171,8 @@ int CHttpServerHandle::ServerInit()
 		return -1;
 	}
 	event_init();
-	httpd = evhttp_start("0.0.0.0", 9002);
-	if (!httpd)
+	m_pEvHttpd = evhttp_start("0.0.0.0", 9002);
+	if (!m_pEvHttpd)
 	{
 		printf("httpd start error\n");
 		GetLogFileHandle().ErrorLog("%s %d httpd start error \n", __FUNCTION__, __LINE__);
@@ -125,12 +184,12 @@ int CHttpServerHandle::ServerInit()
 
 int CHttpServerHandle::Run()
 {
-	evhttp_set_cb(httpd, "/", RootHandler, NULL);
-	evhttp_set_gencb(httpd, GenericHandler, NULL);
-	printf("httpd(%d) server start OK!\n", GetCurrentThreadId());
-	GetLogFileHandle().ErrorLog("%s %d httpd server start OK!\n", __FUNCTION__, __LINE__);
+	evhttp_set_cb(m_pEvHttpd, "/", HttpRootHandler, NULL);
+	evhttp_set_gencb(m_pEvHttpd, HttpGenericHandler, NULL);
+	printf("http server %d start OK!\n", GetCurrentThreadId());
+	GetLogFileHandle().InfoLog("%s %d httpd server start OK!\n", __FUNCTION__, __LINE__);
 	event_dispatch();
-	evhttp_free(httpd);
+	evhttp_free(m_pEvHttpd);
 	WSACleanup();
 	return 0;
 }
--- WinControlCpp/WinControl/libevent_http/libevent_http.h
+++ WinControlCpp/WinControl/libevent_http/libevent_http.h
@@ -9,20 +9,42 @@
 #include <event2/bufferevent.h>
 #include <event2/buffer.h>
 #include <evhttp.h>
+#include <string>
+
+using namespace std;
 
 class CHttpServerHandle 
 {
 public:
 	CHttpServerHandle();
 	~CHttpServerHandle();
-
+	
 	void ServerStart();
 
 private:
-	static void RootHandler(struct evhttp_request *req, void *arg);
-	static void GenericHandler(struct evhttp_request *req, void *arg);
+	//初始化Http
 	int ServerInit();
+	//启动Http线程
 	int Run();
 
-	struct evhttp *httpd;
+
+	//Http其他方式使用HttpDefaultHandler处理
+	static void HttpDefaultHandler(string &strDataBuffer, struct evhttp_request *pEvHttpRequest, void *pArg);
+	//Get方法处理函数
+	static void HttpGetHandler(string& strDataBuffer, struct evhttp_request *pEvHttpRequest, void *pArg);
+	//将不同的Http request请求分发到不同的函数处理
+	static void WebRequestDispatch(UINT32 uiID, Json::Value& jsonJarg, string& strReplyBuffer);
+	//处理Web的request请求并得到返回结果
+	static void WebRequestParser(string& strRequestBuffer, string& strReplyBuffer);
+	//获取post的json数据
+	static void GetHttpPostData(string& strDataBuffer, struct evhttp_request *pEvHttpRequest, void *pArg);
+	//Post方法处理函数
+	static void HttpPostHandler(string& strDataBuffer, struct evhttp_request *pEvHttpRequest, void *pArg);
+	//特定URI回调函数
+	static void HttpRootHandler(struct evhttp_request *pEvHttpRequest, void *pArg);
+	//没有被特定URI回调则调用此函数 
+	static void HttpGenericHandler(struct evhttp_request *pEvHttpRequest, void *pArg);
+
+private:
+	struct evhttp *m_pEvHttpd;
 };
